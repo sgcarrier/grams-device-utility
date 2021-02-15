2bc687f1b84fa7f72f4687e7f37ed52c4b2efa4c
Simon Carrier <simon.g.carrier@usherbrooke.ca>
2021-02-15 16:16:03 -0500

    Added test multicast

--- devices/generic/dma/dma_consumer.py
+++ devices/generic/dma/dma_consumer.py
@@ -14,6 +14,8 @@ class AXIDMA:
             _logger.error("Could not load DMA shared library with error :")
             _logger.error(e)
 
+    def test_multicast(self):
+		self.clib.test_multicast(0x12345678)
 
     def start_data_acquisition(self):
         pass
