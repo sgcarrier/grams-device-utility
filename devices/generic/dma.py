598a99db1fceeadca5d91e90c09449638dbd94c4
Simon Carrier <simon.g.carrier@usherbrooke.ca>
2021-02-23 17:11:37 -0500

    Added acquisition for DMA with multicast

--- devices/generic/dma.py
+++ devices/generic/dma.py
@@ -1,7 +1,7 @@
-import time
-import smbus
+import random
 import logging
 import ctypes, ctypes.util
+from ctypes import *
 
 _logger = logging.getLogger(__name__)
 
@@ -15,10 +15,18 @@ class AXIDMA:
             _logger.error(e)
 
     def test_multicast(self):
-		self.clib.test_multicast(0x0)
+        self.clib.test_multicast(0x0)
 
-    def start_data_acquisition(self):
-        pass
+    def start_data_acquisition(self, maxSamples=0, maxTime=0):
+        _logger.info("Starting acquisition with multicast")
+        acqId = random.randint(1, 65535)
+
+        #The following line is blocking
+        ret = self.clib.start_acquisition_multicast(c_int(maxSamples), c_int(maxTime), c_int(acqId))
+
+        if ret != 0:
+            _logger.error("Acquisition ended with an error...")
 
     def stop_data_acquisition(self):
-        pass
+        self.clib.stop_acquisition()
+        _logger.info("Stopped acquisition")
