17daacc76b0a02703d1bf9672f9050990eed15ac
Simon Carrier <simon.g.carrier@usherbrooke.ca>
2021-02-24 13:56:07 -0500

    Added multicast metadata sending

--- devices/generic/dma.py
+++ devices/generic/dma.py
@@ -1,5 +1,8 @@
 import random
 import logging
+import socket
+import struct
+import sys
 import ctypes, ctypes.util
 from ctypes import *
 
@@ -30,3 +33,19 @@ class AXIDMA:
     def stop_data_acquisition(self):
         self.clib.stop_acquisition()
         _logger.info("Stopped acquisition")
+
+
+    def set_meta_data(self, path, acqID, format, attributes=None):
+
+        message = "ACQ_ID:"+str(acqID)+",PATH:"+path+",FORMAT:"+str(format)+",END:#!"
+        multicast_group = ('238.0.0.8', 19001)
+
+        # Create the datagram socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        ttl = struct.pack('b', 1)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+        sock.sendto(message, multicast_group)
+
+        sock.close()
